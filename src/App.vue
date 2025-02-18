f71fa5145a2b59d0c01bab8c02bef193569e301c
letengzz <2020885569@qq.com>
2025-02-18 21:33:39 +0800

    chore: update locale

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import en from 'element-plus/es/locale/lang/en.mjs'
+import zhCn from 'element-plus/es/locale/lang/zh-cn.mjs'
 import { RouterView } from 'vue-router'
+
+const language = ref('zh-cn')
+const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
 </script>
 
 <template>
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <style scoped>
