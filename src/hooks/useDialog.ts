d40fdec8dcbda0ca9fd223be81e0a0b8b378e895
letengzz <2020885569@qq.com>
2025-02-18 17:36:03 +0800

    chore: update dialog hooks

--- src/hooks/useDialog.ts
+++ src/hooks/useDialog.ts
@@ -1,16 +1,23 @@
-import type { DialogModel } from '@/type/BaseType.ts'
 import { reactive } from 'vue'
+
+interface DialogModel {
+  title: string
+  visible: boolean
+  width: number
+  height: number
+}
 
 export default function useDialog() {
   // 定义弹框属性
   const dialog = reactive<DialogModel>({
-    title: '',
+    title: '弹框',
     visible: false,
     width: 630,
     height: 280,
   })
   // 展示
   const onShow = () => {
+    // 显示数据
     dialog.visible = true
   }
   // 关闭
--- src/type/BaseType.ts
+++
@@ -1,6 +0,0 @@
-export interface DialogModel {
-  title: string
-  visible: boolean
-  width: number
-  height: number
-}
