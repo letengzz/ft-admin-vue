7dd73eca0a93de218df81c11f0f3cc9178f388b1
letengzz <2020885569@qq.com>
2025-03-22 19:04:57 +0800

    chore: update layout index

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
-import Header from "@/layout/components/Header/Header.vue";
-import MenuBar from "@/layout/components/Menu/MenuBar.vue";
+import MenuBar from "./components/Menu/MenuBar.vue";
+import Header from "./components/Header/Header.vue";
 </script>
 
 <template>
-  <el-container class="layout-container">
-    <el-aside width="230px" class="leftmenu">
+  <el-container class="layout-container" h="[100%]">
+    <el-aside width="auto" class="leftmenu">
       <MenuBar />
     </el-aside>
     <el-container>
       <el-header class="header">
         <Header />
       </el-header>
-      <el-main class="content">Main</el-main>
+      <el-main class="content">
+        <RouterView />
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <style scoped lang="scss">
 .layout-container {
-  height: 100%;
-
   .header {
-    background-color: #f5f5f5;
+    display: flex;
+    align-items: center;
+    background-color: #009688;
   }
 
   .leftmenu {
