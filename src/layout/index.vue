12235f0618686aaf3fe5fb3a7434732c0c51a063
letengzz <2020885569@qq.com>
2025-03-22 21:49:36 +0800

    feat: add tabs

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import MenuBar from "./components/Menu/MenuBar.vue";
-import Header from "./components/Header/Header.vue";
+import Header from "@/layout/components/Header/Header.vue";
+import MenuBar from "@/layout/components/Menu/MenuBar.vue";
+import CloseTabs from "@/layout/components/Tabs/CloseTabs.vue";
+import Tabs from "@/layout/components/Tabs/Tabs.vue";
 </script>
 
 <template>
@@ -13,6 +15,10 @@ import Header from "./components/Header/Header.vue";
         <Header />
       </el-header>
       <el-main class="content">
+        <div class="tabs">
+          <Tabs class="tabs-content" />
+          <CloseTabs />
+        </div>
         <RouterView />
       </el-main>
     </el-container>
@@ -30,5 +36,32 @@ import Header from "./components/Header/Header.vue";
   .leftmenu {
     background-color: #304156;
   }
+
+  .content {
+    padding: 0;
+
+    .tabs {
+      display: flex;
+      flex-grow: 1;
+      justify-content: space-between;
+      padding-right: 75px;
+      border-bottom: 1px solid #e4e7ed;
+
+      .el-tabs {
+        width: 100%;
+        height: 35px;
+      }
+
+      .tabs-content {
+        padding-top: 8px;
+        padding-right: 10px;
+        padding-left: 10px;
+      }
+    }
+  }
+}
+
+:deep(.el-tabs__nav > .el-tabs__item) {
+  border-bottom: none !important;
 }
 </style>
